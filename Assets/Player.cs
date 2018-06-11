16a2805d6198ac8fc807ab2bd26ece3acc77139d
antonyadhiban <gtadhib@gmail.com>
2018-06-11 18:39:15 +0530

    Fix input but changing Rigidbody2d to Rigidbody2D

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,15 +6,11 @@ public class Player : MonoBehaviour {
 
 	public float jumpForce = 10f;
 
-	public Rigidbody2d rb;
-	// Use this for initialization
-	void Start () {
-		
-	}
+	public Rigidbody2D rb;
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown("Jump")) || Input.GetMouseButtonDown(0) || Input.GetTouch(1)){
+		if (Input.GetMouseButtonDown(0)){
 			rb.velocity = Vector2.up * jumpForce;
 		}
 	}
